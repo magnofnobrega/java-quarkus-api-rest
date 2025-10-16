cdc80094e6f69d87c0c5f8f638629a01e356de7d
magnofnobrega <magnofnobrega@outlook.com>
2025-10-16 15:14:07 -0300

    fix: colocado o arquivo Produto.java na pasta certa, main. E atualizado conteudo do arquivo ProdutoResource.java - no intelliJ - pasta java verde é teste. e azul é produção

--- src/main/java/com/magno/produtosapi/ProdutoResource.java
+++ src/main/java/com/magno/produtosapi/ProdutoResource.java
@@ -1,16 +1,55 @@
+//package com.magno.produtosapi;
+//
+//import jakarta.ws.rs.GET;
+//import jakarta.ws.rs.Path;
+//import jakarta.ws.rs.Produces;
+//import jakarta.ws.rs.core.MediaType;
+//
+//@Path("/produtos")
+//public class ProdutoResource {
+//
+//    @GET
+//    @Produces(MediaType.TEXT_PLAIN)
+//    public String hello() {
+//        return "Hello from Quarkus REST";
+//    }
+//}
 package com.magno.produtosapi;
-
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/produtos")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class ProdutoResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    public List<Produto> listar() {
+        return Produto.listAll();
+    }
+
+    @POST
+    public void criar (Produto produto) {
+        produto.persist();
+    }
+
+    @PUT
+    @Path("{id}")
+    public void atualizar(@PathParam("id") Long id, Produto produto) {
+        Produto entity = Produto.findById(id);
+        if (entity != null) {
+            entity.nome = produto.nome;
+            entity.preco = produto.preco;
+        }
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void deletar(@PathParam("id") Long id) {
+        Produto entity = Produto.findById(id);
+        if (entity != null) {
+            entity.delete();
+        }
     }
 }
